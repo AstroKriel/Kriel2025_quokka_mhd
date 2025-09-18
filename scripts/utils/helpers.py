407ce7d67e8329af5c3579a5da7aa2d0335023aa
neco kriel <necokriel@gmail.com>
2025-09-18 12:33:35 +1000

    allow user to pass in more args

--- scripts/utils/helpers.py
+++ scripts/utils/helpers.py
@@ -1,39 +1,53 @@
 ## { MODULE
 
 ##
-## === DEPENDENCIES ===
+## === DEPENDENCIES
 ##
 
 import argparse
 from pathlib import Path
 
 ##
-## === FUNCTIONS ===
+## === FUNCTIONS
 ##
 
 
 def get_user_input():
     parser = argparse.ArgumentParser(description="Test Quokka simulation(s).")
     parser.add_argument(
-        "--sim-dir",
-        "-sd",
+        "--dir",
+        "-d",
         type=lambda path: Path(path).expanduser().resolve(),
         default=None,
-        help="Optional path to a Quokka sim-directory.",
+        help="Optional path to a Quokka simulation or dataset directory.",
+    )
+    parser.add_argument(
+        "--components",
+        "-c",
+        nargs="+",
+        default=None,
+        help="Optional list of components to plot.",
+    )
+    parser.add_argument(
+        "--axes",
+        "-a",
+        nargs="+",
+        default=None,
+        help="Optional list of axes to plot.",
     )
     args = parser.parse_args()
-    return args.sim_dir
+    return args
 
 
-def get_latest_plt_dirs(
+def get_latest_dataset_dirs(
     sim_dir: Path,
 ) -> list[Path]:
-    plt_dirs = [
+    dataset_dirs = [
         sub_dir for sub_dir in sim_dir.iterdir()
         if sub_dir.is_dir() and ("plt" in sub_dir.name) and ("old" not in sub_dir.name)
     ]
-    plt_dirs.sort(key=lambda plt_dir: int(plt_dir.name.split("plt")[1]))  # sort by index
-    return plt_dirs
+    dataset_dirs.sort(key=lambda dataset_dir: int(dataset_dir.name.split("plt")[1]))  # sort by index
+    return dataset_dirs
 
 
 ## } MODULE
