2e1b89c67cb432ae5a022366c58aea5380ddc723
neco kriel <necokriel@gmail.com>
2025-09-27 14:39:02 +1000

    generalise slice plotting for vfields

--- scripts/plot_b_slice.py
+++ scripts/plot_b_slice.py
@@ -6,7 +6,8 @@
 
 import numpy
 from jormi.ww_plots import plot_manager, plot_data, annotate_axis
-from utils import helpers, load_quokka_dataset
+from ww_quokka_sims.sim_io import load_dataset
+from utils import helpers
 
 ##
 ## === HELPERS
@@ -37,13 +38,9 @@ def _slice_sfield(
     sfield,
     axis_to_slice: str,
 ):
-    nx, ny, nz = sfield.shape
-    ix = 0 # nx // 2
-    iy = 0 # ny // 2
-    iz = 0 # nz // 2
-    if axis_to_slice == "z": return sfield[:, :, iz], r"$(x, y, z=0)$"
-    if axis_to_slice == "y": return sfield[:, iy, :], r"$(x, y=0, z)$"
-    if axis_to_slice == "x": return sfield[ix, :, :], r"$(x=0, y, z)$"
+    if axis_to_slice == "z": return sfield[:, :, 0], r"$(x, y, z=0)$"
+    if axis_to_slice == "y": return sfield[:, 0, :], r"$(x, y=0, z)$"
+    if axis_to_slice == "x": return sfield[0, :, :], r"$(x=0, y, z)$"
     raise ValueError("axis_to_slice must be one of: x, y, z")
 
 
@@ -53,56 +50,46 @@ def _plot_slice(
     domain,
     component: str,
     axis_to_slice: str,
+    comp_labels: dict[str, str],
+    cmap_name: str,
 ):
     component_to_index = {"x": 0, "y": 1, "z": 2}
-    component_labels = {"x": r"$|b_x|$", "y": r"$|b_y|$", "z": r"$|b_z|$"}
     sfield = vfield.data[component_to_index[component]]
-    sfield_slice, label = _slice_sfield(sfield, axis_to_slice)
+    sfield_slice, slice_label = _slice_sfield(sfield, axis_to_slice)
+    min_value = numpy.min(sfield_slice)
+    max_value = numpy.max(sfield_slice)
     plot_data.plot_sfield_slice(
         ax=ax,
         field_slice=sfield_slice,
         axis_bounds=_compute_slice_bounds(domain, axis_to_slice),
-        cmap_name="cmr.arctic",
+        cmap_name=cmap_name,
         add_colorbar=True,
-        cbar_label=component_labels[component],
+        cbar_label=comp_labels[component],
         cbar_side="right",
+        cbar_bounds=(min_value, max_value),
     )
     annotate_axis.add_text(
         ax=ax,
-        x_pos = 0.5,
-        y_pos = 0.95,
+        x_pos=0.5,
+        y_pos=0.95,
         x_alignment="center",
         y_alignment="top",
-        label=f"({numpy.min(sfield_slice):.2e}, {numpy.max(sfield_slice):.2e})",
+        label=f"({min_value:.2e}, {max_value:.2e})",
         fontsize=16,
         box_alpha=0.5,
         add_box=True,
     )
     annotate_axis.add_text(
         ax=ax,
-        x_pos = 0.5,
-        y_pos = 0.05,
+        x_pos=0.5,
+        y_pos=0.05,
         x_alignment="center",
         y_alignment="bottom",
-        label=label,
+        label=slice_label,
         fontsize=16,
         box_alpha=0.5,
         add_box=True,
     )
-
-
-def _cast_axs_to_grid(
-    axs,
-    num_rows: int,
-    num_cols: int,
-):
-    if (num_rows == 1) and (num_cols == 1):
-        return [[axs]]
-    if num_rows == 1:
-        return [list(axs)]
-    if num_cols == 1:
-        return [[ax] for ax in axs]
-    return axs
 
 
 ##
@@ -112,45 +99,88 @@ def _cast_axs_to_grid(
 
 class Plotter:
 
+    VALID_FIELDS = {
+        "mag": {
+            "loader": "load_magnetic_vfield",
+            "labels": {
+                "x": r"$b_x$",
+                "y": r"$b_y$",
+                "z": r"$b_z$"
+            },
+            "cmap": "Blues",
+            "fig_prefix": "b_slice_",
+        },
+        "vel": {
+            "loader": "load_velocity_vfield",
+            "labels": {
+                "x": r"$v_x$",
+                "y": r"$v_y$",
+                "z": r"$v_z$"
+            },
+            "cmap": "Oranges",
+            "fig_prefix": "v_slice_",
+        },
+    }
+
     def __init__(
         self,
         input_dir,
+        fields: list[str],
         components_to_plot: list[str],
         axes_to_slice: list[str],
     ):
         valid_axes = {"x", "y", "z"}
+        valid_fields = set(self.VALID_FIELDS.keys())
+        if not fields or not set(fields).issubset(valid_fields):
+            raise ValueError(f"Provide one or more fields (via -f) from: {sorted(valid_fields)}")
         if not axes_to_slice or not set(axes_to_slice).issubset(valid_axes):
             raise ValueError("Provide one or more axes (via -a) from: x, y, z")
         if not components_to_plot or not set(components_to_plot).issubset(valid_axes):
             raise ValueError("Provide one or more components (via -c) from: x, y, z")
         self.input_dir = input_dir
+        self.fields = list(fields)
         self.axes_to_slice = list(axes_to_slice)
         self.components_to_plot = list(components_to_plot)
-        self.component_labels = {"x": r"$|b_x|$", "y": r"$|b_y|$", "z": r"$|b_z|$"}
 
     def run(
         self,
     ) -> None:
-        dataset_dirs, is_single_dataset = self._resolve_dataset_dirs(self.input_dir)
-        if is_single_dataset:
-            fig_dir = dataset_dirs[0].parent
-            self._plot_snapshot(
-                dataset_dir=dataset_dirs[0],
-                fig_dir=fig_dir
-            )
-        else:
-            fig_dir = self.input_dir
-            for dataset_dir in dataset_dirs:
+        dataset_dirs = helpers.resolve_dataset_dirs(self.input_dir)
+        for field in self.fields:
+            if len(dataset_dirs) == 1:
+                fig_dir = dataset_dirs[0].parent
                 self._plot_snapshot(
-                    dataset_dir=dataset_dir,
-                    fig_dir=fig_dir
+                    field=field,
+                    dataset_dir=dataset_dirs[0],
+                    fig_dir=fig_dir,
                 )
+            else:
+                fig_dir = self.input_dir
+                for dataset_dir in dataset_dirs:
+                    self._plot_snapshot(
+                        field=field,
+                        dataset_dir=dataset_dir,
+                        fig_dir=fig_dir,
+                    )
+
+    def _load_vfield(
+        self,
+        ds,
+        field: str,
+    ):
+        specs = self.VALID_FIELDS[field]
+        loader_name = specs["loader"]
+        return getattr(ds, loader_name)()
 
     def _plot_snapshot(
         self,
+        field: str,
         dataset_dir,
         fig_dir,
     ) -> None:
+        specs = self.VALID_FIELDS[field]
+        comp_labels = specs["labels"]
+        cmap_name = specs["cmap"]
         num_rows = len(self.components_to_plot)
         num_cols = len(self.axes_to_slice)
         fig, axs = plot_manager.create_figure(
@@ -159,9 +189,9 @@ class Plotter:
             y_spacing=0.35,
             x_spacing=0.25,
         )
-        axs_grid = _cast_axs_to_grid(axs, num_rows, num_cols)
-        with load_quokka_dataset.QuokkaDataset(dataset_dir=dataset_dir) as dataset:
-            vfield = dataset.load_magnetic_field()
+        axs_grid = helpers.get_axs_grid(axs, num_rows, num_cols)
+        with load_dataset.QuokkaDataset(dataset_dir=dataset_dir) as dataset:
+            vfield = self._load_vfield(dataset, field)
             domain = dataset.load_domain()
         for row_index, component in enumerate(self.components_to_plot):
             for col_index, axis_to_slice in enumerate(self.axes_to_slice):
@@ -172,23 +202,15 @@ class Plotter:
                     domain=domain,
                     component=component,
                     axis_to_slice=axis_to_slice,
+                    comp_labels=comp_labels,
+                    cmap_name=cmap_name,
                 )
         self._style_slice_axes(axs_grid)
-        label = dataset_dir.name.split("plt")[1]
-        fig_name = f"b_slice_{label}.png"
+        index_label = dataset_dir.name.split("plt")[1]
+        fig_prefix = specs["fig_prefix"]
+        fig_name = f"{fig_prefix}{index_label}.png"
         fig_path = fig_dir / fig_name
         plot_manager.save_figure(fig=fig, fig_path=fig_path)
-
-    @staticmethod
-    def _resolve_dataset_dirs(
-        input_dir,
-    ):
-        if "plt" in input_dir.name:
-            dataset_dirs = [input_dir]
-            return dataset_dirs, True
-        dataset_dirs = helpers.get_latest_dataset_dirs(sim_dir=input_dir)
-        assert len(dataset_dirs) != 0
-        return dataset_dirs, False
 
     def _style_slice_axes(
         self,
@@ -210,9 +232,10 @@ class Plotter:
 def main():
     args = helpers.get_user_input()
     plotter = Plotter(
-        input_dir=args.dir,
-        components_to_plot=args.components,
-        axes_to_slice=args.axes,
+      input_dir=args.dir,
+      fields=args.fields,
+      components_to_plot=args.components,
+      axes_to_slice=args.axes,
     )
     plotter.run()
 
